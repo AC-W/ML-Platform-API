887aca107b7c3e58bf5dfb3b0e3ab2f0209c2d4f
AC-W <weiaocheng2014@gmail.com>
2022-09-05 11:16:50 -0400

    url image link added

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 from PIL import Image
+import requests
 from io import BytesIO
 from module.loadstore import FileManager as fm
 
@@ -54,16 +55,41 @@ def load_car_model():
         car_net = torchvision.models.resnet50(pretrained=False)
         num_ftrs = car_net.fc.in_features
         car_net.fc = nn.Linear(num_ftrs, 1513)
-        state = torch.hub.load_state_dict_from_url("https://github.com/AC-W/ML-Platform-Server/raw/main/Models/CarRecognitionModel",map_location=torch.device('cpu'))
+        state = torch.hub.load_state_dict_from_url("https://github.com/AC-W/ML-Platform-API/raw/main/Models/CarRecognitionModel",map_location=torch.device('cpu'))
         car_net.load_state_dict(state)
         return {"msg":"car_model loaded"}
 
 
 # predict car model
 @app.post('/predict_car_model')
-async def predict_car_image(file: UploadFile = File(...)):
+async def predict_car_image(file: UploadFile = File(...),url : str = File()):
+    global car_net
     if (car_net):
         img = Image.open(BytesIO(await file.read()))
+        show_rank_num = 5
+        img = transform(img)
+        with torch.no_grad():
+            car_net.eval()
+            all = car_net(img.unsqueeze(0))
+            p = torch.nn.functional.softmax(all[0], dim=0)
+            _, top = torch.topk(all,show_rank_num)
+            top = top.tolist()
+            name = []
+            prob = []
+            for i in range(len(top[0])):
+                index = top[0][i]
+                name.append(f'{car_labels.get(index)}')
+                prob.append(f'{p[index]*100:.2f}%')
+        return {"models":name,"probs":prob}
+    else:
+        return {"msg":'model not loaded'}
+
+@app.post('/predict_car_model_url')
+async def predict_car_image(url: str = File()):
+    global car_net
+    if (car_net):
+        response = requests.get(url)
+        img = Image.open(BytesIO(response.content))
         show_rank_num = 5
         img = transform(img)
         with torch.no_grad():
